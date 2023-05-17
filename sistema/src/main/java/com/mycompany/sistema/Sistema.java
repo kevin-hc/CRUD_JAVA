e30a310daac70e3cc2949d697219087e2f49a22c
kevin-hc <122648942+kevin-hc@users.noreply.github.com>
2023-05-17 19:21:56 -0400

    Add files via upload

---
+++ sistema/src/main/java/com/mycompany/sistema/Sistema.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema;
+
+/**
+ *
+ * @author kevin
+ */
+public class Sistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
+
